5eb0aa63c0000affc7d34ed5fa49420ede10a12f
Shaik Asif <bhanurshaikasif@gmail.com>
2025-01-02 19:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase to handle case insensitivity
+  const lowerStr = s1.toLowerCase();
+  const lowerSubStr = s2.toLowerCase();
+  
+  // Use indexOf to find the first occurrence of the lowercase version of subStr in str
+  return lowerStr.indexOf(lowerSubStr);
 }
 
 // Please do not change the code below
